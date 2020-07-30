c71061ec25dca439ee43f17c428809fbb469a2f8
elija <elijahesmero@gmail.com>
2020-07-30 17:44:36 +1000

    Complete calculatingWithFunctions (poor refactoring)

---
+++ calculatingWithFunctions/calculatingWithFunctions.test.js
@@ -0,0 +1,114 @@
+// https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39
+
+function zero(args) {
+    if (args === undefined) {
+        return '0'
+    } else {
+        return eval('~~(0' + args + ')');
+    }
+}
+
+function one(args) {
+    if (args === undefined) {
+        return '1'
+    } else {
+        return eval('~~(1' + args + ')');
+    }
+}
+
+function two(args) {
+    if (args === undefined) {
+        return '2'
+    } else {
+        return eval('~~(2' + args + ')');
+    }
+}
+
+function three(args) {
+    if (args === undefined) {
+        return '3'
+    } else {
+        return eval('~~(3' + args + ')');
+    }
+}
+
+function four(args) {
+    if (args === undefined) {
+        return '4'
+    } else {
+        return eval('~~(4' + args + ')');
+    }
+}
+
+function five(args) {
+    if (args === undefined) {
+        return '5'
+    } else {
+        return eval('~~(5' + args + ')');
+    }
+}
+
+function six(args) {
+    if (args === undefined) {
+        return '6'
+    } else {
+        return eval('~~(6' + args + ')');
+    }
+}
+
+function seven(args) {
+    if (args === undefined) {
+        return '7'
+    } else {
+        return eval('~~(7' + args + ')');
+    }
+}
+
+function eight(args) {
+    if (args === undefined) {
+        return '8'
+    } else {
+        return eval('~~(8' + args + ')');
+    }
+}
+
+function nine(args) {
+    if (args === undefined) {
+        return '9'
+    } else {
+        return eval('~~(9' + args + ')');
+    }
+}
+
+function plus(args) {
+    if (args === undefined) {
+        return '+'
+    } else {
+        return '+' + args;
+    }
+}
+
+function minus(args) {
+    if (args === undefined) {
+        return '-'
+    } else {
+        return '-' + args;
+    }
+}
+
+function times(args) {
+    if (args === undefined) {
+        return '*'
+    } else {
+        return '*' + args;
+    }
+}
+
+function dividedBy(args) {
+    if (args === undefined) {
+        return '/'
+    } else {
+        return '/' + args;
+    }
+}
+
