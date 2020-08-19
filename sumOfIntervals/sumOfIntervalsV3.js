4f357cbfe7067655f7041801b79e0fa475310e54
elijahjorell <elijahesmero@gmail.com>
2020-08-19 17:55:04 +1000

    Complete sumOfIntervals

--- sumOfIntervals/sumOfIntervalsV3.js
+++ sumOfIntervals/sumOfIntervalsV3.js
@@ -3,53 +3,38 @@
 const intervals = [[1, 4], [7, 10], [3, 5]];
 
 function sumIntervals(intervals) {
-    var newIntervals = [].concat(intervals);
-    var pairs = [];
-    var k = 0;
-    var reset = false;
-
-    while (true) {
-        console.log(newIntervals)
-        for (var i = 0; i < newIntervals.length; i++) {
-            for (var j = i + 1; j < newIntervals.length; j++) {
-                pairs.push([newIntervals[i],newIntervals[j]].sort((a, b) => a[0] - b[0]));
-            }
-        }
+    var currentIntervals = [].concat(intervals);
+    var reset;
+    var pair;
+    var spread;
     
-        while (k < pairs.length) {
-            if (checkOverlap(pairs[k])) {
-                pairs[k].map((val) => {
-                    newIntervals.splice(newIntervals.indexOf(val), 1);
-                });
-                newIntervals.push([pairs[k][0][0], pairs[k][1][1]])
-                pairs.splice(k, 1);
-                k = -1;
-                reset = true;
+    while (true) {
+        reset = false;
+        for (var i = 0; i < currentIntervals.length; i++) {
+            for (var j = i + 1; j < currentIntervals.length; j++) {
+                pair = [currentIntervals[i], currentIntervals[j]].sort((a, b) => a[0] - b[0]);
+                spread = [].concat(...pair).sort((a, b) => a - b);
+                
+                if (pair[0][1] === spread[3] || // interval A encapsulates interval B
+                    pair[0][1] === pair[1][0] || // interval A and interval B touch
+                    pair[0][1] !== spread[1]) { // interval A and interval B overlap
+                        currentIntervals.push([spread[0], spread[3]]);
+                        pair.map((val) => { currentIntervals.splice(currentIntervals.indexOf(val), 1) });
+                        reset = true;
+                        i = currentIntervals.length;
+                        j = currentIntervals.length;
+                }
             }
-            k++
         }
-
         if (!reset) {
             break;
-        } else {
-            reset = false;
-            pairs = [];
         }
     }
 
-    return newIntervals.reduce((acc, val) => {
+    return currentIntervals.reduce((acc, val) => {
         acc += val[1] - val[0];
         return acc
-    }, 0)   
-}
-
-function checkOverlap(intervalPair) {
-    return intervalPair[0].reduce((acc, val) => {
-        if (val >= intervalPair[1][0] && val <= intervalPair[1][1]) {
-            acc = true;
-        }
-        return acc;
-    }, false);
+    }, 0)
 }
 
 sumIntervals(intervals);
